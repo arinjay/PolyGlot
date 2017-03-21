8401978edb6566e47050af6560209d20864865cb
arinjay <arinjaysharma421@gmail.com>
2017-03-21 14:05:36 +0530

    Loading examples

--- Polyglot/ViewController.swift
+++ Polyglot/ViewController.swift
@@ -10,10 +10,71 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    var words = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        if let defaults = UserDefaults(suiteName: "group.com.Arinjay.Polyglot"){
+            
+            if let saveWords = defaults.object(forKey: "words") as? [String]{
+                words = saveWords
+            }
+            else{
+              saveInitialWords(to: defaults)
+            }
+        }
     }
+    
+    func saveInitialWords(to default: UserDefaults){
+        words.append("Hi::Hallo")
+        words.append("Time::Zeit")
+        words.append("Chicken::Hähnchen")
+        words.append("Sir::Herr")
+        words.append("Brother::Bruder")
+        
+    }
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+   
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return words.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        let word = words[indexPath.row]
+        let split = word.components(separatedBy: "::")
+        cell.textLabel?.text = split[0]
+        
+        cell.detailTextLabel?.text = ""
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+       tableView.deselectRow(at: indexPath, animated: true)
+    
+        if let cell = tableView.cellForRow(at: indexPath){
+            if cell.detailTextLabel?.text == ""{
+                let word = words[indexPath.row]
+                let split = word.components(separatedBy: "::")
+                cell.detailTextLabel?.text = split[1]
+            }else{
+                cell.detailTextLabel?.text = ""
+            }
+    }
+}
+    
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
